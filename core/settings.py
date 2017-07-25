159244ce16882b768588e5510a88ccc39fd6f463
zacky ffx <zacky ffx>
2017-07-25 13:41:15 +0800

    Added a Procfile

--- core/settings.py
+++ core/settings.py
@@ -129,6 +129,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
+#STATICFILES_DIRS = [
+#    STATIC_DIR,
+#]
