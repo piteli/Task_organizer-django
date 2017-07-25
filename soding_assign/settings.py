9400e8b98ffc99f305ed39b5e77feee6cbba9038
zacky ffx <zacky ffx>
2017-07-25 17:36:06 +0800

    fixing

--- soding_assign/settings.py
+++ soding_assign/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'v-=r=q(mhx&l_c&1jhgmug7@8k+jvklp3rcyirjhim!2l236p@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = []
 
 
 # Application definition
